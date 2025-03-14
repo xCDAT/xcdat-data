de7c635a01b91a9f2784201023488e97e38c7135
Tom Vo <tomvothecoder@gmail.com>
2025-03-14 16:44:06 -0700

    Refactor script and update README.md

--- README.md
+++ README.md
@@ -5,13 +5,17 @@ that would bloat the [main repository](https://github.com/xCDAT/xcdat) if includ
 
 ## Available Datasets
 
+Here's your updated table with the new entries:
+
 | Key                 | Filename                                                                 | Description                                                  | Subset                     | Approx. Filesize |
 | ------------------- | ------------------------------------------------------------------------ | ------------------------------------------------------------ | -------------------------- | ---------------- |
 | `pr_amon_access`    | `pr_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412_subset.nc`  | Monthly precipitation data from the ACCESS-ESM1-5 model.     | `1870-01-01 to 1874-12-31` | 7.0 MB           |
 | `tas_amon_access`   | `tas_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412_subset.nc` | Monthly near-surface air temperature from ACCESS-ESM1-5.     | `1870-01-01 to 1874-12-31` | 7.0 MB           |
 | `tas_amon_canesm5`  | `tas_Amon_CanESM5_historical_r13i1p1f1_gn_185001-201412_subset.nc`       | Monthly near-surface air temperature from the CanESM5 model. | `1870-01-01 to 1874-12-31` | 2.0 MB           |
-| `so_omon_cesm2`     | `so_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412_subset.nc`           | Monthly ocean salinity data from the CESM2 model.            | First three time points    | 29 MB            |
-| `thetao_omon_cesm2` | `thetao_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412_subset.nc`       | Monthly ocean potential temperature from the CESM2 model.    | First three time points    | 37 MB            |
+| `so_omon_cesm2`     | `so_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412_subset.nc`           | Monthly ocean salinity data from the CESM2 model.            | First three time points    | 900 MB           |
+| `thetao_omon_cesm2` | `thetao_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412_subset.nc`       | Monthly ocean potential temperature from the CESM2 model.    | First three time points    | 950 MB           |
+| `cl_amon_e3sm2`     | `cl_Amon_E3SM-2-0_historical_r1i1p1f1_gr_185001-189912_subset.nc`        | Monthly cloud fraction data from the E3SM-2-0 model.         | `1870-01-01 to 1874-12-31` | 54 MB            |
+| `ta_amon_e3sm2`     | `ta_Amon_E3SM-2-0_historical_r1i1p1f1_gr_185001-189912_subset.nc`        | Monthly air temperature data from the E3SM-2-0 model.        | `1870-01-01 to 1874-12-31` | 37 MB            |
 
 ## Usage
 
--- scripts/get_datasets.py
+++ scripts/get_datasets.py
@@ -5,26 +5,23 @@ conda create -n xcdat-data dask xarray netcdf4 ipykernel
 # %%
 import xarray as xr
 
-dataset_paths = [
-    "https://esgf-data1.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/CSIRO/ACCESS-ESM1-5/historical/r10i1p1f1/Amon/tas/gn/v20200605/tas_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412.nc",
-    "https://esgf-data1.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/CSIRO/ACCESS-ESM1-5/historical/r10i1p1f1/Amon/pr/gn/v20200605/pr_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412.nc",
-    "http://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/CCCma/CanESM5/historical/r13i1p1f1/Amon/tas/gn/v20190429/tas_Amon_CanESM5_historical_r13i1p1f1_gn_185001-201412.nc",
-]
 
-start_date = "1870-01-01"
-end_date = "1874-12-31"
-
-successes = []
-failures = []
-for path in dataset_paths:
+def process_dataset(
+    path, start_date=None, end_date=None, chunking=None, subset_slice=None
+):
     try:
         print(f"Attempting to open dataset: {path}")
 
-        # Open the dataset
-        ds = xr.open_dataset(path)
+        # Open the dataset with optional chunking
+        ds = xr.open_dataset(path, chunks=chunking)
 
-        # Subset the dataset for the specified time range (assuming the time coordinate is named 'time')
-        ds_sub = ds.sel(time=slice(start_date, end_date))
+        # Subset the dataset for the specified time range or slice
+        if start_date and end_date:
+            ds_sub = ds.sel(time=slice(start_date, end_date))
+        elif subset_slice:
+            ds_sub = ds.isel(time=subset_slice)
+        else:
+            ds_sub = ds
 
         # Define the output file name
         output_file = path.split("/")[-1].replace(".nc", "_subset.nc")
@@ -37,56 +34,85 @@ for path in dataset_paths:
         dataset_size = ds_sub.nbytes / (1024**2)  # Convert bytes to megabytes
         print(f"Size of the dataset: {dataset_size:.2f} MB")
 
-        successes.append(path)
+        return True
     except ValueError as e:
         print(f"Failed to process {path}: {e}")
-        failures.append(path)
+        return False
+
+
+def process_datasets(
+    dataset_paths, start_date=None, end_date=None, chunking=None, subset_slice=None
+):
+    successes = []
+    failures = []
+    for path in dataset_paths:
+        if process_dataset(path, start_date, end_date, chunking, subset_slice):
+            successes.append(path)
+        else:
+            failures.append(path)
+    return successes, failures
+
+
 # %%
+# Regular datasets
+# ----------------
+dataset_paths = [
+    "https://esgf-data1.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/CSIRO/ACCESS-ESM1-5/historical/r10i1p1f1/Amon/tas/gn/v20200605/tas_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412.nc",
+    "https://esgf-data1.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/CSIRO/ACCESS-ESM1-5/historical/r10i1p1f1/Amon/pr/gn/v20200605/pr_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412.nc",
+    "http://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/CCCma/CanESM5/historical/r13i1p1f1/Amon/tas/gn/v20190429/tas_Amon_CanESM5_historical_r13i1p1f1_gn_185001-201412.nc",
+]
+
+start_date = "1870-01-01"
+end_date = "1874-12-31"
+
+successes, failures = process_datasets(dataset_paths, start_date, end_date)
+
 print("Successful datasets:")
 for dataset in successes:
     print(dataset)
 
-# %%
 print("Failed datasets:")
 for dataset in failures:
     print(dataset)
 
 # %%
-# Big datasets
+# E3SM datasets
 # ----------------
-# The below file is 17.3 GB -- it is downloaded manually via http and subsetted locally: https://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/NCAR/CESM2/historical/r1i1p1f1/Omon/so/gn/v20190308/so_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc
-# "http://aims3.llnl.gov/thredds/dodsC/css03_data/CMIP6/CMIP/NCAR/CESM2/historical/r1i1p1f1/Omon/so/gn/v20190308/so_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc"
-# https://aims3.llnl.gov/thredds/fileServer/css03_data/CMIP6/CMIP/NCAR/CESM2/historical/r1i1p1f1/Omon/thetao/gn/v20190308/thetao_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc
 
+e3sm_dataset_paths = [
+    "https://esgf-data2.llnl.gov/thredds/dodsC/user_pub_work/CMIP6/CMIP/E3SM-Project/E3SM-2-0/historical/r1i1p1f1/Amon/ta/gr/v20220830/ta_Amon_E3SM-2-0_historical_r1i1p1f1_gr_185001-189912.nc",
+    "https://esgf-data2.llnl.gov/thredds/dodsC/user_pub_work/CMIP6/CMIP/E3SM-Project/E3SM-2-0/historical/r1i1p1f1/Amon/cl/gr/v20220830/cl_Amon_E3SM-2-0_historical_r1i1p1f1_gr_185001-189912.nc",
+]
+
+successes, failures = process_datasets(
+    e3sm_dataset_paths, chunking={"time": 20}, subset_slice=slice(0, 3)
+)
+
+print("Successful datasets:")
+for dataset in successes:
+    print(dataset)
+
+print("Failed datasets:")
+for dataset in failures:
+    print(dataset)
+
+# Local datasets (too big to use OpenDAP)
+# ----------------------------------------
 local_dataset_paths = [
     "so_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc",
     "thetao_Omon_CESM2_historical_r1i1p1f1_gn_185001-201412.nc",
 ]
-# %%
-for path in local_dataset_paths:
-    try:
-        print(f"Attempting to open big dataset: {path}")
 
-        # Open the big dataset with chunking
-        ds_big = xr.open_dataset(path, chunks={"time": 20})
+successes, failures = process_datasets(
+    local_dataset_paths, chunking={"time": 20}, subset_slice=slice(0, 3)
+)
 
-        # Subset the dataset for the first 3 time slices
-        ds_sub = ds_big.isel(time=slice(0, 3))
+print("Successful datasets:")
+for dataset in successes:
+    print(dataset)
 
-        # Define the output file name
-        output_file = path.split("/")[-1].replace(".nc", "_subset.nc")
-
-        # Save the subset dataset to a netCDF file
-        ds_sub.to_netcdf(output_file)
-        print(f"Saved big dataset to {output_file}")
-
-        # Print the size of the subset dataset
-        dataset_size = ds_sub.nbytes / (1024**2)  # Convert bytes to megabytes
-        print(f"Size of the subset dataset: {dataset_size:.2f} MB")
-
-        successes.append(path)
-    except ValueError as e:
-        print(f"Failed to process {path}: {e}")
-        failures.append(path)
+print("Failed datasets:")
+for dataset in failures:
+    print(dataset)
 
 # %%
